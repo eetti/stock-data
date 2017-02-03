4e77736b14272d73d53733f570404d67e5feb3c4
Kedar Iyer <kedarmail@gmail.com>
2017-02-03 15:32:42 -0500

    sp500 tickers

--- get-volatility-data.py
+++ get-volatility-data.py
@@ -4,7 +4,7 @@ import time
 page = requests.get('http://www.optionstrategist.com/calculators/free-volatility-data')
 data_lines = page.text.split('\n')[177:4905]
 data_lines = data_lines[0:998] + data_lines[1003:2002] + data_lines[2007:3006] + data_lines[3011:4010] + data_lines[4015:]
-data_lines = [line for line in data_lines if 'SERIAL' not in line]
+data_lines = [line for line in data_lines if 'SERIAL' not in line and '@VX' not in line]
 data_array = [line.replace('/ ', ' ').replace('%ile', '').split() for line in data_lines]
 for line in data_array:
     line[4] = line[4][0:2] + '-' + line[4][2:4] + '-' + line[4][4:]
@@ -14,7 +14,6 @@ header = "Symbol,hv20,hv50,hv100,date,iv,days_calculated,percentile,close\n"
 csv = header + '\n'.join(data_lines_comma)
 current_date = time.strftime("%d_%m_%Y")
 filename = 'volatility_{}.csv'.format(current_date)
-print(csv)
 with open(filename, 'w') as f:
     f.write(csv)
 
